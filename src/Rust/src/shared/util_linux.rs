684874738e8c71515012f2e160f7a5cf7eef5985
Caelan <git@caesay.com>
2024-01-12 11:10:11 +0000

    Much simpler Linux apply implementation

--- src/Rust/src/shared/util_linux.rs
+++ src/Rust/src/shared/util_linux.rs
@@ -1,10 +1,7 @@
 use super::bundle::Manifest;
 use crate::shared::bundle;
 use anyhow::{anyhow, bail, Result};
-use std::fs::File;
-use std::io::{Read, Write};
 use std::{path::Path, path::PathBuf, process::Command as Process, time::Duration};
-use std::os::unix::io::FromRawFd;
 
 pub fn wait_for_parent_to_exit(ms_to_wait: u32) -> Result<()> {
     let id = std::os::unix::process::parent_id();
@@ -22,39 +19,6 @@ pub fn wait_for_parent_to_exit(ms_to_wait: u32) -> Result<()> {
     Ok(())
 }
 
-pub fn copy_own_fd_to_file(output_path: &str) -> Result<()> {
-    let path = std::ffi::CString::new("/proc/self/exe")?;
-    let fd = unsafe { libc::open(path.as_ptr(), libc::O_RDONLY) };
-    let result = copy_fd_to_file(fd, &output_path);
-    unsafe { libc::close(fd) };
-    result?;
-    Ok(())
-}
-
-fn copy_fd_to_file(fd: i32, output_path: &str) -> Result<()> {
-    // SAFETY: Assuming the FD is valid and open for reading
-    let mut source_file = unsafe { File::from_raw_fd(fd) };
-    let mut target_file = File::create(output_path)?;
- 
-    // Buffer to hold data while copying
-    let mut buffer = [0; 4096];
- 
-    loop {
-        let bytes_read = source_file.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break; // End of file reached
-        }
-        target_file.write_all(&buffer[..bytes_read])?;
-    }
- 
-    Ok(())
-}
-
-// pub fn force_stop_package<P: AsRef<Path>>(_root_dir: P) -> Result<()> {
-//     // not supported on linux / no-op
-//     Ok(())
-// }
-
 pub fn start_package<P: AsRef<Path>>(_app: &Manifest, root_dir: P, exe_args: Option<Vec<&str>>, set_env: Option<&str>) -> Result<()> {
     let mut cmd = Process::new(root_dir.as_ref());
     if let Some(args) = exe_args {
@@ -67,29 +31,12 @@ pub fn start_package<P: AsRef<Path>>(_app: &Manifest, root_dir: P, exe_args: Opt
     Ok(())
 }
 
-// pub fn detect_manifest_from_update_path(update_exe: &PathBuf) -> Result<(PathBuf, Manifest)> {
-//     let mut manifest_path = update_exe.clone();
-//     manifest_path.pop();
-//     manifest_path.push("sq.version");
-//     let manifest = load_manifest(&manifest_path)?;
+pub fn detect_current_manifest() -> Result<(PathBuf, Manifest)> {
+    let mut manifest_path = std::env::current_exe()?;
+    manifest_path.pop();
+    manifest_path.push("sq.version");
+    let manifest = load_manifest(&manifest_path)?;
 
-//     let my_path = std::env::current_exe()?;
-//     let my_path = my_path.to_string_lossy();
-//     let app_idx = my_path.find("/usr/bin/");
-//     if app_idx.is_none() {
-//         bail!("Unable to find /usr/bin/ directory in path: {}", my_path);
-//     }
-
-//     let root_dir = &my_path[..app_idx.unwrap()];
-
-//     debug!("Detected Root: {}", root_dir);
-//     debug!("Detected AppId: {}", manifest.id);
-//     Ok((Path::new(&root_dir).to_path_buf(), manifest))
-// }
-
-pub fn detect_current_manifest(package: &PathBuf) -> Result<(PathBuf, Manifest)> {
-    let bundle = bundle::load_bundle_from_file(package)?;
-    let manifest = bundle.read_manifest()?;
     let path = std::env::var("APPIMAGE")?;
     let path = Path::new(&path).to_path_buf();
     if !path.exists() {
@@ -98,11 +45,11 @@ pub fn detect_current_manifest(package: &PathBuf) -> Result<(PathBuf, Manifest)>
     Ok((path, manifest))
 }
 
-// fn load_manifest(nuspec_path: &PathBuf) -> Result<Manifest> {
-//     if Path::new(&nuspec_path).exists() {
-//         if let Ok(nuspec) = super::retry_io(|| std::fs::read_to_string(&nuspec_path)) {
-//             return Ok(bundle::read_manifest_from_string(&nuspec)?);
-//         }
-//     }
-//     bail!("Unable to read nuspec file in current directory.")
-// }
+fn load_manifest(nuspec_path: &PathBuf) -> Result<Manifest> {
+    if Path::new(&nuspec_path).exists() {
+        if let Ok(nuspec) = super::retry_io(|| std::fs::read_to_string(&nuspec_path)) {
+            return Ok(bundle::read_manifest_from_string(&nuspec)?);
+        }
+    }
+    bail!("Unable to read nuspec file in current directory.")
+}
