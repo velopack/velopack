015a10ccc1b1e5eaaf6e54276ba1ea54a46e352e
caesay <git@caesay.com>
2024-01-23 22:03:46 +0000

    Skip unware test

--- test/Velopack.Packaging.Tests/WindowsPackTests.cs
+++ test/Velopack.Packaging.Tests/WindowsPackTests.cs
@@ -162,7 +162,7 @@ public class WindowsPackTests
         Assert.Throws<UserInfoException>(() => runner.Run(options).GetAwaiterResult());
     }
 
-    [SkippableFact]
+    [SkippableFact(Skip = "Need to sort out test binary architecture")]
     public void PackRefuseBuildingUnawareApp()
     {
         Skip.IfNot(VelopackRuntimeInfo.IsWindows);
