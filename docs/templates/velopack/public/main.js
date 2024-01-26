d8baed8ee77f5072958a6e36d9c2ecbeab74b801
Caelan Sayler <git@caesay.com>
2024-01-26 19:07:54 +0000

    Add docfx template

---
+++ docs/templates/velopack/public/main.js
@@ -0,0 +1,9 @@
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/velopack/velopack',
+            title: 'GitHub'
+        }
+    ]
+}
