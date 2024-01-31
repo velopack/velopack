8c27d9b3487a816bc1d187cb082d1e8c25241abf
Caelan Sayler <git@caesay.com>
2024-01-31 16:49:28 +0000

    Should exit right away after restarting for updates

--- src/Velopack/VelopackApp.cs
+++ src/Velopack/VelopackApp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.Versioning;
@@ -214,6 +214,7 @@ namespace Velopack
                 if (!restarted && _autoApply) {
                     log.Info("Auto apply is true, so restarting to apply update...");
                     UpdateExe.Apply(locator, true, true, args, log);
+                    Exit(0);
                 } else {
                     log.Info("Pre-condition failed, we will not restart to apply updates. (restarted: " + restarted + ", autoApply: " + _autoApply + ")");
                 }
