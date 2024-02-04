da9e244037f4b0a4c88460584ac7e5433f246963
Caelan Sayler <git@caesay.com>
2024-02-04 00:07:09 +0000

    fix: install splash not respecting silent flag

--- src/Rust/src/commands/install.rs
+++ src/Rust/src/commands/install.rs
@@ -111,9 +111,16 @@ pub fn install(debug_pkg: Option<&PathBuf>, install_to: Option<&PathBuf>) -> Res
     info!("Preparing and cleaning installation directory...");
     remove_dir_all::ensure_empty_dir(&root_path)?;
 
-    info!("Reading splash image...");
-    let splash_bytes = pkg.get_splash_bytes();
-    let tx = windows::splash::show_splash_dialog(app.title.to_owned(), splash_bytes);
+    let tx = if dialogs::get_silent() {
+        info!("Will not show splash because silent mode is on.");
+        let (tx, _) = std::sync::mpsc::channel::<i16>();
+        tx
+    } else {
+        info!("Reading splash image...");
+        let splash_bytes = pkg.get_splash_bytes();
+        windows::splash::show_splash_dialog(app.title.to_owned(), splash_bytes)
+    };
+    
     let install_result = install_impl(&pkg, &root_path, &tx);
     let _ = tx.send(windows::splash::MSG_CLOSE);
 
