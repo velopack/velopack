004e25514b7079040cf43e85fd1fc1432bc639b8
Caelan Sayler <git@caesay.com>
2024-02-05 18:42:38 +0000

    moved asciinema to another repo

--- asciinema/do.sh
+++
@@ -1,9 +0,0 @@
-. ../../asciinema/demo-magic.sh
-
-alias vpk='../../build/Debug/net8.0/vpk'
-
-clear
-pei "dotnet publish -r osx-x64 -c Release --self-contained -o publish"
-echo " "
-pei "vpk pack -u AvaloniaCrossPlat -v 1.0.1 -p publish -i Velopack.icns"
-exit
