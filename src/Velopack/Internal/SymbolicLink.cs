4d53f96ce2376aaa317a106dfbe4bbd546529f00
Caelan Sayler <git@caesay.com>
2024-03-19 15:07:05 +0000

    Simpler symbolic link implementation

---
+++ src/Velopack/Internal/SymbolicLink.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace Velopack
+{
+    internal static class SymbolicLink
+    {
+        /// <summary>
+        /// Creates a symlink from the specified directory to the specified target directory.
+        /// </summary>
+        /// <param name="linkPath">The symlink path</param>
+        /// <param name="targetPath">The target directory or file</param>
+        /// <param name="overwrite">If true overwrites an existing reparse point or empty directory</param>
+        public static void Create(string linkPath, string targetPath, bool overwrite = true)
+        {
+            targetPath = Path.GetFullPath(targetPath);
+
+            if (!Directory.Exists(targetPath) && !File.Exists(targetPath)) {
+                throw new IOException("Target path does not exist.");
+            }
+
+            if (Directory.Exists(linkPath) || File.Exists(linkPath)) {
+                if (overwrite) {
+                    Utility.DeleteFileOrDirectoryHard(linkPath);
+                } else {
+                    throw new IOException("Junction already exists and overwrite parameter is false.");
+                }
+            }
+
+            if (Directory.Exists(targetPath)) {
+#if NETFRAMEWORK
+                if (!CreateSymbolicLink(linkPath, targetPath, SYMBOLIC_LINK_FLAG_DIRECTORY | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE))
+                    ThrowLastWin32Error("Unable to create junction point / symlink.");
+#else
+                Directory.CreateSymbolicLink(linkPath, targetPath);
+#endif
+            } else if (File.Exists(targetPath)) {
+#if NETFRAMEWORK
+                if (!CreateSymbolicLink(linkPath, targetPath, SYMBOLIC_LINK_FLAG_FILE | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE))
+                    ThrowLastWin32Error("Unable to create junction point / symlink.");
+#else
+                File.CreateSymbolicLink(linkPath, targetPath);
+#endif
+            } else {
+                throw new IOException("Target path does not exist.");
+            }
+        }
+
+        public static bool Exists(string linkPath)
+        {
+            return TryGetLinkFsi(linkPath, out var _);
+        }
+
+        public static void Delete(string linkPath)
+        {
+            var isLink = TryGetLinkFsi(linkPath, out var fsi);
+            if (fsi != null && !isLink) {
+                throw new IOException("Path is not a junction point.");
+            } else {
+                fsi?.Delete();
+            }
+        }
+
+        public static string GetTarget(string linkPath)
+        {
+            if (TryGetLinkFsi(linkPath, out var fsi)) {
+#if NETFRAMEWORK
+                return GetTargetWin32(linkPath);
+#else
+                return fsi.LinkTarget!;
+#endif
+            }
+            throw new IOException("Path does not exist or is not a junction point / symlink.");
+        }
+
+        private static bool TryGetLinkFsi(string path, out FileSystemInfo fsi)
+        {
+            fsi = null!;
+            if (Directory.Exists(path)) {
+                fsi = new DirectoryInfo(path);
+            } else if (File.Exists(path)) {
+                fsi = new FileInfo(path);
+            }
+
+            return fsi != null && (fsi.Attributes & FileAttributes.ReparsePoint) != 0;
+        }
+
+        public static string GetRelativePath(string relativeTo, string path)
+        {
+#if NETFRAMEWORK
+            relativeTo = Path.GetFullPath(relativeTo);
+            path = Path.GetFullPath(path);
+            return ToggleRelative(relativeTo, path);
+#else
+            return Path.GetRelativePath(relativeTo, path);
+#endif
+        }
+
+#if NETFRAMEWORK
+        [Flags]
+        private enum EFileAccess : uint
+        {
+            GenericRead = 0x80000000,
+            GenericWrite = 0x40000000,
+            GenericExecute = 0x20000000,
+            GenericAll = 0x10000000,
+        }
+
+        [Flags]
+        private enum EFileShare : uint
+        {
+            None = 0x00000000,
+            Read = 0x00000001,
+            Write = 0x00000002,
+            Delete = 0x00000004,
+        }
+
+        private enum ECreationDisposition : uint
+        {
+            New = 1,
+            CreateAlways = 2,
+            OpenExisting = 3,
+            OpenAlways = 4,
+            TruncateExisting = 5,
+        }
+
+        [Flags]
+        private enum EFileAttributes : uint
+        {
+            Readonly = 0x00000001,
+            Hidden = 0x00000002,
+            System = 0x00000004,
+            Directory = 0x00000010,
+            Archive = 0x00000020,
+            Device = 0x00000040,
+            Normal = 0x00000080,
+            Temporary = 0x00000100,
+            SparseFile = 0x00000200,
+            ReparsePoint = 0x00000400,
+            Compressed = 0x00000800,
+            Offline = 0x00001000,
+            NotContentIndexed = 0x00002000,
+            Encrypted = 0x00004000,
+            Write_Through = 0x80000000,
+            Overlapped = 0x40000000,
+            NoBuffering = 0x20000000,
+            RandomAccess = 0x10000000,
+            SequentialScan = 0x08000000,
+            DeleteOnClose = 0x04000000,
+            BackupSemantics = 0x02000000,
+            PosixSemantics = 0x01000000,
+            OpenReparsePoint = 0x00200000,
+            OpenNoRecall = 0x00100000,
+            FirstPipeInstance = 0x00080000
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr CreateFile(
+            string lpFileName,
+            EFileAccess dwDesiredAccess,
+            EFileShare dwShareMode,
+            IntPtr lpSecurityAttributes,
+            ECreationDisposition dwCreationDisposition,
+            EFileAttributes dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+
+        private const int SYMBOLIC_LINK_FLAG_FILE = 0x0;
+        private const int SYMBOLIC_LINK_FLAG_DIRECTORY = 0x1;
+        private const int SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE = 0x2;
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern uint GetFinalPathNameByHandle(IntPtr hFile, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszFilePath, uint cchFilePath, uint dwFlags);
+
+        private static string GetTargetWin32(string linkPath)
+        {
+            using var handle = new SafeFileHandle(CreateFile(linkPath, EFileAccess.GenericRead,
+                EFileShare.Read | EFileShare.Write | EFileShare.Delete,
+                IntPtr.Zero, ECreationDisposition.OpenExisting,
+                EFileAttributes.BackupSemantics, IntPtr.Zero), true);
+
+            if (Marshal.GetLastWin32Error() != 0)
+                ThrowLastWin32Error("Unable to open reparse point.");
+
+            var sb = new StringBuilder(1024);
+            var res = GetFinalPathNameByHandle(handle.DangerousGetHandle(), sb, 1024, 0);
+            if (res == 0)
+                ThrowLastWin32Error("Unable to resolve reparse point target.");
+
+            var result = sb.ToString();
+            if (result.StartsWith(@"\\?\"))
+                result = result.Substring(4);
+            if (result.StartsWith(@"\\?\UNC\"))
+                result = @"\\" + result.Substring(8);
+            return result;
+        }
+
+        private static void ThrowLastWin32Error(string message)
+        {
+            throw new IOException(message, Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error()));
+        }
+
+        private static string ToggleRelative(string basePath, string toggledPath)
+        {
+            // from https://github.com/RT-Projects/RT.Util/blob/master/RT.Util.Core/Paths/PathUtil.cs#L297
+            if (basePath.Length == 0)
+                throw new Exception("InvalidBasePath");
+            if (toggledPath.Length == 0)
+                throw new Exception("InvalidToggledPath");
+            if (!Path.IsPathRooted(basePath))
+                throw new Exception("BasePathNotAbsolute");
+
+            try { basePath = Path.GetFullPath(basePath + "\\"); } catch { throw new Exception("InvalidBasePath"); }
+
+            if (!Path.IsPathRooted(toggledPath))
+                try {
+                    return StripTrailingSeparator(Path.GetFullPath(Path.Combine(basePath, toggledPath)));
+                } catch {
+                    throw new Exception("InvalidToggledPath");
+                }
+
+            // Both basePath and toggledPath are absolute. Need to relativize toggledPath.
+            try { toggledPath = Path.GetFullPath(toggledPath + "\\"); } catch { throw new Exception("InvalidToggledPath"); }
+            int prevPos = -1;
+            int pos = toggledPath.IndexOf(Path.DirectorySeparatorChar);
+            while (pos != -1 && pos < basePath.Length && basePath.Substring(0, pos + 1).Equals(toggledPath.Substring(0, pos + 1), StringComparison.OrdinalIgnoreCase)) {
+                prevPos = pos;
+                pos = toggledPath.IndexOf(Path.DirectorySeparatorChar, pos + 1);
+            }
+            if (prevPos == -1)
+                throw new Exception("PathsOnDifferentDrives");
+            var piece = basePath.Substring(prevPos + 1);
+            var result = StripTrailingSeparator((".." + Path.DirectorySeparatorChar).Repeat(piece.Count(ch => ch == Path.DirectorySeparatorChar))
+                + toggledPath.Substring(prevPos + 1));
+            return result.Length == 0 ? "." : result;
+        }
+
+        private static string Repeat(this string input, int numTimes)
+        {
+            if (numTimes == 0) return "";
+            if (numTimes == 1) return input;
+            if (numTimes == 2) return input + input;
+            var sb = new StringBuilder();
+            for (int i = 0; i < numTimes; i++)
+                sb.Append(input);
+            return sb.ToString();
+        }
+
+        private static string StripTrailingSeparator(string path)
+        {
+            if (path.Length < 1)
+                return path;
+            if (path[path.Length - 1] == '/' || path[path.Length - 1] == '\\')
+                return (path.Length == 3 && path[1] == ':') ? path : path.Substring(0, path.Length - 1);
+            else
+                return path;
+        }
+#endif
+    }
+}
--- test/Velopack.Tests/SymbolicLinkTests.cs
+++ test/Velopack.Tests/SymbolicLinkTests.cs
@@ -1,12 +1,12 @@
 ﻿namespace Velopack.Tests;
 
-public class JunctionPointTests
+public class SymbolicLinkTests
 {
     [Fact]
     public void Exists_NoSuchFile()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
-        Assert.False(JunctionPoint.Exists(Path.Combine(tempFolder, "$$$NoSuchFolder$$$")));
+        Assert.False(SymbolicLink.Exists(Path.Combine(tempFolder, "$$$NoSuchFolder$$$")));
     }
 
     [Fact]
@@ -15,11 +15,11 @@ public class JunctionPointTests
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
         File.Create(Path.Combine(tempFolder, "AFile")).Close();
 
-        Assert.False(JunctionPoint.Exists(Path.Combine(tempFolder, "AFile")));
+        Assert.False(SymbolicLink.Exists(Path.Combine(tempFolder, "AFile")));
     }
 
     [Fact]
-    public void Create_VerifyExists_GetTarget_Delete()
+    public void CreateDirectory_VerifyExists_GetTarget_Delete()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
         string targetFolder = Path.Combine(tempFolder, "ADirectory");
@@ -32,22 +32,22 @@ public class JunctionPointTests
         Assert.False(File.Exists(Path.Combine(junctionPoint, "AFile")),
             "File should not be located until junction point created.");
 
-        Assert.False(JunctionPoint.Exists(junctionPoint), "Junction point not created yet.");
+        Assert.False(SymbolicLink.Exists(junctionPoint), "Junction point not created yet.");
 
         // Create junction point and confirm its properties.
-        JunctionPoint.Create(junctionPoint, targetFolder, false /*don't overwrite*/);
+        SymbolicLink.Create(junctionPoint, targetFolder, false /*don't overwrite*/);
 
-        Assert.True(JunctionPoint.Exists(junctionPoint), "Junction point exists now.");
+        Assert.True(SymbolicLink.Exists(junctionPoint), "Junction point exists now.");
 
-        Assert.Equal(targetFolder, JunctionPoint.GetTarget(junctionPoint));
+        Assert.Equal(targetFolder, SymbolicLink.GetTarget(junctionPoint));
 
         Assert.True(File.Exists(Path.Combine(junctionPoint, "AFile")),
             "File should be accessible via the junction point.");
 
         // Delete junction point.
-        JunctionPoint.Delete(junctionPoint);
+        SymbolicLink.Delete(junctionPoint);
 
-        Assert.False(JunctionPoint.Exists(junctionPoint), "Junction point should not exist now.");
+        Assert.False(SymbolicLink.Exists(junctionPoint), "Junction point should not exist now.");
 
         Assert.False(File.Exists(Path.Combine(junctionPoint, "AFile")),
             "File should not be located after junction point deleted.");
@@ -59,6 +59,34 @@ public class JunctionPointTests
     }
 
     [Fact]
+    public void CreateFile_VerifyExists_GetTarget_Delete()
+    {
+        using var _1 = Utility.GetTempDirectory(out var tempFolder);
+        var tmpFile = Path.Combine(tempFolder, "AFile");
+        var symFile = Path.Combine(tempFolder, "SymFile");
+        File.Create(tmpFile).Close();
+
+        Assert.False(File.Exists(symFile), "File should not be located until junction point created.");
+        Assert.False(SymbolicLink.Exists(symFile), "File should not be located until junction point created.");
+
+        SymbolicLink.Create(symFile, tmpFile, true);
+
+        Assert.True(File.Exists(symFile), "Symfile point exists now.");
+        Assert.True(SymbolicLink.Exists(symFile), "Junction point exists now.");
+
+        Assert.Equal(tmpFile, SymbolicLink.GetTarget(symFile));
+
+        // verify symlink contents match real file.
+        Assert.Empty(File.ReadAllBytes(symFile));
+        File.WriteAllText(tmpFile, "Hello, World!");
+        Assert.Equal("Hello, World!", File.ReadAllText(symFile));
+
+        SymbolicLink.Delete(symFile);
+        Assert.False(File.Exists(symFile));
+        Assert.False(SymbolicLink.Exists(symFile));
+    }
+
+    [Fact]
     public void Create_ThrowsIfOverwriteNotSpecifiedAndDirectoryExists()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
@@ -66,7 +94,7 @@ public class JunctionPointTests
         string junctionPoint = Path.Combine(tempFolder, "SymLink");
 
         Directory.CreateDirectory(junctionPoint);
-        Assert.Throws<IOException>(() => JunctionPoint.Create(junctionPoint, targetFolder, false));
+        Assert.Throws<IOException>(() => SymbolicLink.Create(junctionPoint, targetFolder, false));
     }
 
     [Fact]
@@ -79,9 +107,9 @@ public class JunctionPointTests
         Directory.CreateDirectory(junctionPoint);
         Directory.CreateDirectory(targetFolder);
 
-        JunctionPoint.Create(junctionPoint, targetFolder, true);
+        SymbolicLink.Create(junctionPoint, targetFolder, true);
 
-        Assert.Equal(targetFolder, JunctionPoint.GetTarget(junctionPoint));
+        Assert.Equal(targetFolder, SymbolicLink.GetTarget(junctionPoint));
     }
 
     [Fact]
@@ -90,21 +118,21 @@ public class JunctionPointTests
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
         string targetFolder = Path.Combine(tempFolder, "ADirectory");
         string junctionPoint = Path.Combine(tempFolder, "SymLink");
-        Assert.Throws<IOException>(() => JunctionPoint.Create(junctionPoint, targetFolder, false));
+        Assert.Throws<IOException>(() => SymbolicLink.Create(junctionPoint, targetFolder, false));
     }
 
     [Fact]
     public void GetTarget_NonExistentJunctionPoint()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
-        Assert.Throws<IOException>(() => JunctionPoint.GetTarget(Path.Combine(tempFolder, "SymLink")));
+        Assert.Throws<IOException>(() => SymbolicLink.GetTarget(Path.Combine(tempFolder, "SymLink")));
     }
 
     [Fact]
     public void GetTarget_CalledOnADirectoryThatIsNotAJunctionPoint()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
-        Assert.Throws<IOException>(() => JunctionPoint.GetTarget(tempFolder));
+        Assert.Throws<IOException>(() => SymbolicLink.GetTarget(tempFolder));
     }
 
     [Fact]
@@ -113,7 +141,7 @@ public class JunctionPointTests
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
         File.Create(Path.Combine(tempFolder, "AFile")).Close();
 
-        Assert.Throws<IOException>(() => JunctionPoint.GetTarget(Path.Combine(tempFolder, "AFile")));
+        Assert.Throws<IOException>(() => SymbolicLink.GetTarget(Path.Combine(tempFolder, "AFile")));
     }
 
     [Fact]
@@ -121,14 +149,14 @@ public class JunctionPointTests
     {
         // Should do nothing.
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
-        JunctionPoint.Delete(Path.Combine(tempFolder, "SymLink"));
+        SymbolicLink.Delete(Path.Combine(tempFolder, "SymLink"));
     }
 
     [Fact]
     public void Delete_CalledOnADirectoryThatIsNotAJunctionPoint()
     {
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
-        Assert.Throws<IOException>(() => JunctionPoint.Delete(tempFolder));
+        Assert.Throws<IOException>(() => SymbolicLink.Delete(tempFolder));
     }
 
     [Fact]
@@ -137,6 +165,6 @@ public class JunctionPointTests
         using var _1 = Utility.GetTempDirectory(out var tempFolder);
         File.Create(Path.Combine(tempFolder, "AFile")).Close();
 
-        Assert.Throws<IOException>(() => JunctionPoint.Delete(Path.Combine(tempFolder, "AFile")));
+        Assert.Throws<IOException>(() => SymbolicLink.Delete(Path.Combine(tempFolder, "AFile")));
     }
 }
