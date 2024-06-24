02ff2fda5174438a1d0d22514b1a6bbfa6e8ff6d
Kevin Bost <kitokeboo@gmail.com>
2024-06-24 00:50:00 -0700

    Updating Uno sample
    Updating Uno sample with latest changes.

--- samples/UnoSample/UnoSample/Platforms/Desktop/Program.cs
+++ samples/UnoSample/UnoSample/Platforms/Desktop/Program.cs
@@ -1,5 +1,4 @@
 using Uno.UI.Runtime.Skia;
-using Velopack;
 
 namespace UnoSample;
 public class Program
@@ -7,11 +6,6 @@ public class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        // It's important to Run() the VelopackApp as early as possible in app startup.
-        VelopackApp.Build()
-            .WithFirstRun((v) => { /* Your first run code here */ })
-            .Run();
-
 #if (!useDependencyInjection && useLoggingFallback)
         App.InitializeLogging();
 
