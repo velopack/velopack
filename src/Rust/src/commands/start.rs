925f8d03f0e44ba1de3f053edf099a937ddce148
Caelan Sayler <git@caesay.com>
2024-07-04 17:04:52 +0100

    unix compiler warnings

--- src/Rust/src/commands/start.rs
+++ src/Rust/src/commands/start.rs
@@ -1,10 +1,11 @@
 use std::path::PathBuf;
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 
 use crate::bundle::Manifest;
 use crate::shared::{self, OperationWait};
 
+#[allow(unused_variables, unused_imports)]
 pub fn start(
     root_dir: &PathBuf,
     app: &Manifest,
@@ -13,6 +14,8 @@ pub fn start(
     exe_args: Option<Vec<&str>>,
     legacy_args: Option<&String>,
 ) -> Result<()> {
+    use anyhow::bail;
+
     #[cfg(target_os = "windows")]
     if legacy_args.is_some() && exe_args.is_some() {
         bail!("Cannot use both legacy args and new args format.");
