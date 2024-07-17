c0a9476c22e18b4a5c7ce8cb1b68d1deaaa83db9
Caelan <git@caesay.com>
2024-07-17 12:53:20 +0100

    Properly detect universal binaries when packing .app bundles (#179)

--- src/Velopack.Packaging.Unix/BinDetect.cs
+++ src/Velopack.Packaging.Unix/BinDetect.cs
@@ -7,7 +7,11 @@ public class BinDetect
         MH_MAGIC = 0xfeedface,
         MH_CIGAM = 0xcefaedfe,
         MH_MAGIC_64 = 0xfeedfacf,
-        MH_CIGAM_64 = 0xcffaedfe
+        MH_CIGAM_64 = 0xcffaedfe,
+        // https://developer.apple.com/documentation/kernel/fat_header/1558632-magic/
+        // https://opensource.apple.com/source/file/file-80.40.2/file/magic/Magdir/cafebabe.auto.html
+        FAT_MAGIC = 0xcafebabe,
+        FAT_CIGAM = 0xbebafeca,
     }
 
     public static bool IsMachOImage(string filePath)
