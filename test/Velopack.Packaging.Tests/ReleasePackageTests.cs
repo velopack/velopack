8fb1469e6eda941b3044e635afcc056465fa20c8
Marco Franzen <Marco.Franzen@orgadata.com>
2024-08-28 16:47:16 +0200

    fix semver version check - 0.0.1 is now allowed with a release label

--- test/Velopack.Packaging.Tests/ReleasePackageTests.cs
+++ test/Velopack.Packaging.Tests/ReleasePackageTests.cs
@@ -23,33 +23,6 @@
 //        {
 //        }
 
-//        [Theory]
-//        [InlineData("1.2.3")]
-//        [InlineData("1.2.3-alpha13")]
-//        [InlineData("1.2.3-alpha135")]
-//        [InlineData("0.0.1")]
-//        [InlineData("1.299656.3-alpha")]
-//        public void SemanticVersionParsesValidVersion(string ver)
-//        {
-//            NugetUtil.ThrowIfVersionNotSemverCompliant(ver);
-//            Assert.True(SemanticVersion.TryParse(ver, out var _));
-//        }
-
-//        [Theory]
-//        [InlineData("")]
-//        [InlineData("1")]
-//        [InlineData("0")]
-//        [InlineData("1.2.3.4")]
-//        [InlineData("1.2.3.4-alpha")]
-//        [InlineData("0.0.0.0")]
-//        [InlineData("0.0.0")]
-//        [InlineData("0.0")]
-//        [InlineData("0.0.0-alpha")]
-//        public void SemanticVersionThrowsInvalidVersion(string ver)
-//        {
-//            Assert.ThrowsAny<Exception>(() => NugetUtil.ThrowIfVersionNotSemverCompliant(ver));
-//        }
-
 //        [Fact]
 //        public void ReleasePackageIntegrationTest()
 //        {
---
+++ test/Velopack.Tests/NugetUtilTests.cs
@@ -0,0 +1,36 @@
+﻿using NuGet.Versioning;
+using Velopack.NuGet;
+
+namespace Velopack.Tests;
+
+public class NugetUtilTests
+{
+    [Theory]
+    [InlineData("1.2.3")]
+    [InlineData("1.2.3-alpha13")]
+    [InlineData("1.2.3-alpha135")]
+    [InlineData("0.0.1")]
+    [InlineData("0.0.1-beta")]
+    [InlineData("0.0.1-beta01")]
+    [InlineData("1.299656.3-alpha")]
+    public void SemanticVersionParsesValidVersion(string ver)
+    {
+        NugetUtil.ThrowIfVersionNotSemverCompliant(ver);
+        Assert.True(SemanticVersion.TryParse(ver, out var _));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("1")]
+    [InlineData("0")]
+    [InlineData("1.2.3.4")]
+    [InlineData("1.2.3.4-alpha")]
+    [InlineData("0.0.0.0")]
+    [InlineData("0.0.0")]
+    [InlineData("0.0")]
+    [InlineData("0.0.0-alpha")]
+    public void SemanticVersionThrowsInvalidVersion(string ver)
+    {
+        Assert.ThrowsAny<Exception>(() => NugetUtil.ThrowIfVersionNotSemverCompliant(ver));
+    }
+}
