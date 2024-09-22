6c06f7e09cc6b10e65f99dd95938547094ca2860
Caelan <git@caesay.com>
2024-09-22 11:43:04 -0600

    remove another invalid appimage argument!

--- src/vpk/Velopack.Packaging.Unix/AppImageTool.cs
+++ src/vpk/Velopack.Packaging.Unix/AppImageTool.cs
@@ -61,7 +61,6 @@ public class AppImageTool
                     "zstd",
                     "-root-owned",
                     "-noappend",
-                    "100%",
                     "-b",
                     "16384",
                     "-mkfs-time",
