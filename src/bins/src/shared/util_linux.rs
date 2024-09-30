5af0d06cd6d5a0f091d6ffb54fb1590ed4f30a6e
Caelan <git@caesay.com>
2024-09-30 09:52:09 -0600

    Initial Linux migration

--- src/bins/src/shared/util_linux.rs
+++ src/bins/src/shared/util_linux.rs
@@ -1,7 +1,6 @@
-use super::bundle::Manifest;
-use crate::shared::bundle;
 use anyhow::{anyhow, bail, Result};
 use std::{path::Path, path::PathBuf, process::Command as Process, time::Duration};
+use velopack::locator::VelopackLocator;
 
 pub fn wait_for_pid_to_exit(pid: u32, ms_to_wait: u32) -> Result<()> {
     info!("Waiting {}ms for process ({}) to exit.", ms_to_wait, pid);
@@ -24,8 +23,9 @@ pub fn wait_for_parent_to_exit(ms_to_wait: u32) -> Result<()> {
     Ok(())
 }
 
-pub fn start_package<P: AsRef<Path>>(_app: &Manifest, root_dir: P, exe_args: Option<Vec<&str>>, set_env: Option<&str>) -> Result<()> {
-    let mut cmd = Process::new(root_dir.as_ref());
+pub fn start_package(locator: &VelopackLocator, exe_args: Option<Vec<&str>>, set_env: Option<&str>) -> Result<()> {
+    let root_dir = locator.get_root_dir();
+    let mut cmd = Process::new(root_dir);
     if let Some(args) = exe_args {
         cmd.args(args);
     }
@@ -34,27 +34,4 @@ pub fn start_package<P: AsRef<Path>>(_app: &Manifest, root_dir: P, exe_args: Opt
     }
     cmd.spawn().map_err(|z| anyhow!("Failed to start_package ({}).", z))?;
     Ok(())
-}
-
-pub fn detect_current_manifest() -> Result<(PathBuf, Manifest)> {
-    let mut manifest_path = std::env::current_exe()?;
-    manifest_path.pop();
-    manifest_path.push("sq.version");
-    let manifest = load_manifest(&manifest_path)?;
-
-    let path = std::env::var("APPIMAGE")?;
-    let path = Path::new(&path).to_path_buf();
-    if !path.exists() {
-        bail!("Unable to find AppImage at: {}", path.to_string_lossy());
-    }
-    Ok((path, manifest))
-}
-
-fn load_manifest(nuspec_path: &PathBuf) -> Result<Manifest> {
-    if Path::new(&nuspec_path).exists() {
-        if let Ok(nuspec) = super::retry_io(|| std::fs::read_to_string(&nuspec_path)) {
-            return Ok(bundle::read_manifest_from_string(&nuspec)?);
-        }
-    }
-    bail!("Unable to read nuspec file in current directory.")
 }
