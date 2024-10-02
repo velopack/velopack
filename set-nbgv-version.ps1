1a61ef588e23926cf46929c46f2442dc60eeeda3
Caelan <git@caesay.com>
2024-10-02 16:06:09 -0600

    Simplify nbgv version setting

---
+++ set-nbgv-version.ps1
@@ -0,0 +1,19 @@
+$scriptDir = $PSScriptRoot
+$path = Join-Path $scriptDir "Cargo.toml"
+$version = (nbgv get-version -v NuGetPackageVersion).Trim()
+Write-Host "Setting version to $version"
+
+(Get-Content $path) | ForEach-Object {
+    if ($_ -match '^version\s*=\s*".*"') {
+        $_ -replace '^version\s*=\s*".*"', "version = `"$version`""
+    }
+    else {
+        $_
+    }
+} | Set-Content $path
+
+cargo update --offline
+cargo pkgid -p velopack
+
+Set-Location "$scriptDir/src/lib-nodejs"
+npm version $version --no-git-tag-version
