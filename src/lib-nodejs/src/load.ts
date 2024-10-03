9b1b8ee6e48d7a2631e5d32a4e62de464d5ce665
Caelan <git@caesay.com>
2024-10-02 20:37:36 -0600

    try createRequire

--- src/lib-nodejs/src/load.ts
+++ src/lib-nodejs/src/load.ts
@@ -1,14 +1,17 @@
-import { proxy } from "@neon-rs/load";
+import {proxy} from "@neon-rs/load";
+import {createRequire} from "module";
+
+export const nodeRequire = createRequire(__filename);
 
 module.exports = proxy({
-  platforms: {
-    "win32-x86-msvc": () => require("./native/velopack_nodeffi_win_x86_msvc.node"),
-    "win32-x64-msvc": () => require("./native/velopack_nodeffi_win_x64_msvc.node"),
-    "win32-arm64-msvc": () => require("./native/velopack_nodeffi_win_arm64_msvc.node"),
-    "darwin-x64": () => require("./native/velopack_nodeffi_osx.node"),
-    "darwin-arm64": () => require("./native/velopack_nodeffi_osx.node"),
-    "linux-x64-gnu": () => require("./native/velopack_nodeffi_linux_x64_gnu.node"),
-    "linux-arm64-gnu": () => require("./native/velopack_nodeffi_linux_arm64_gnu.node"),
-  },
-  debug: () => require("../index.node"),
+    platforms: {
+        "win32-x86-msvc": () => nodeRequire("./native/velopack_nodeffi_win_x86_msvc.node"),
+        "win32-x64-msvc": () => nodeRequire("./native/velopack_nodeffi_win_x64_msvc.node"),
+        "win32-arm64-msvc": () => nodeRequire("./native/velopack_nodeffi_win_arm64_msvc.node"),
+        "darwin-x64": () => nodeRequire("./native/velopack_nodeffi_osx.node"),
+        "darwin-arm64": () => nodeRequire("./native/velopack_nodeffi_osx.node"),
+        "linux-x64-gnu": () => nodeRequire("./native/velopack_nodeffi_linux_x64_gnu.node"),
+        "linux-arm64-gnu": () => nodeRequire("./native/velopack_nodeffi_linux_arm64_gnu.node"),
+    },
+    debug: () => nodeRequire("../index.node"),
 });
