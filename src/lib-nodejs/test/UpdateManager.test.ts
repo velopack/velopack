5fd48be265816f33a5339829e4c76d5f047cb4b6
Caelan <git@caesay.com>
2024-10-07 11:46:52 -0600

    refactor js test logging

--- src/lib-nodejs/test/UpdateManager.test.ts
+++ src/lib-nodejs/test/UpdateManager.test.ts
@@ -7,88 +7,81 @@ import {
     setVelopackLogger,
 } from "../src";
 import path from "path";
-import {tempd3, fixture, updateExe, shortDelay} from "./helper";
+import {tempd3, fixture, updateExe, captureLogs} from "./helper";
 
-function testInit() {
-    VelopackApp.build().run();
-    setVelopackLogger((level, msg) => {
-        console.log(level, msg);
-    });
-}
+test("UpdateManager detects local update", async () => {
+    await captureLogs(async () => {
+        await tempd3(async (tmpDir, packagesDir, rootDir) => {
+            const locator: VelopackLocatorConfig = {
+                ManifestPath: "../../test/fixtures/Test.Squirrel-App.nuspec",
+                PackagesDir: packagesDir,
+                RootAppDir: rootDir,
+                UpdateExePath: updateExe(),
+                CurrentBinaryDir: path.join(rootDir, "current"),
+                IsPortable: true,
+            };
 
-test("UpdateManager detects local update", () => {
-    return tempd3(async (tmpDir, packagesDir, rootDir) => {
-        testInit();
-        const locator: VelopackLocatorConfig = {
-            ManifestPath: "../../test/fixtures/Test.Squirrel-App.nuspec",
-            PackagesDir: packagesDir,
-            RootAppDir: rootDir,
-            UpdateExePath: updateExe(),
-            CurrentBinaryDir: path.join(rootDir, "current"),
-            IsPortable: true,
-        };
+            const options: UpdateOptions = {
+                ExplicitChannel: "beta",
+                AllowVersionDowngrade: false,
+            };
 
-        const options: UpdateOptions = {
-            ExplicitChannel: "beta",
-            AllowVersionDowngrade: false,
-        };
+            const um = new UpdateManager(tmpDir, options, locator);
+            copyFileSync(
+                fixture("testfeed.json"),
+                path.join(tmpDir, "releases.beta.json"),
+            );
+            const update = await um.checkForUpdatesAsync();
 
-        const um = new UpdateManager(tmpDir, options, locator);
-        copyFileSync(
-            fixture("testfeed.json"),
-            path.join(tmpDir, "releases.beta.json"),
-        );
-        const update = await um.checkForUpdatesAsync();
-
-        expect(update).not.toBeNull();
-        expect(update?.TargetFullRelease).not.toBeNull();
-        expect(update?.TargetFullRelease?.Version).toBe("1.0.11");
-        expect(update?.TargetFullRelease?.FileName).toBe(
-            "AvaloniaCrossPlat-1.0.11-full.nupkg",
-        );
-        await shortDelay();
+            expect(update).not.toBeNull();
+            expect(update?.TargetFullRelease).not.toBeNull();
+            expect(update?.TargetFullRelease?.Version).toBe("1.0.11");
+            expect(update?.TargetFullRelease?.FileName).toBe(
+                "AvaloniaCrossPlat-1.0.11-full.nupkg",
+            );
+        });
     });
 });
 
-test("UpdateManager downloads full update", () => {
-    return tempd3(async (feedDir, packagesDir, rootDir) => {
-        testInit();
-        const locator: VelopackLocatorConfig = {
-            ManifestPath: "../../test/fixtures/Test.Squirrel-App.nuspec",
-            PackagesDir: packagesDir,
-            RootAppDir: rootDir,
-            UpdateExePath: updateExe(),
-            CurrentBinaryDir: path.join(rootDir, "current"),
-            IsPortable: true,
-        };
+test("UpdateManager downloads full update", async () => {
+    await captureLogs(async () => {
+        await tempd3(async (feedDir, packagesDir, rootDir) => {
+            const locator: VelopackLocatorConfig = {
+                ManifestPath: "../../test/fixtures/Test.Squirrel-App.nuspec",
+                PackagesDir: packagesDir,
+                RootAppDir: rootDir,
+                UpdateExePath: updateExe(),
+                CurrentBinaryDir: path.join(rootDir, "current"),
+                IsPortable: true,
+            };
 
-        const options: UpdateOptions = {
-            ExplicitChannel: "beta",
-            AllowVersionDowngrade: false,
-        };
+            const options: UpdateOptions = {
+                ExplicitChannel: "beta",
+                AllowVersionDowngrade: false,
+            };
 
-        const um = new UpdateManager(feedDir, options, locator);
-        copyFileSync(
-            fixture("testfeed.json"),
-            path.join(feedDir, "releases.beta.json"),
-        );
+            const um = new UpdateManager(feedDir, options, locator);
+            copyFileSync(
+                fixture("testfeed.json"),
+                path.join(feedDir, "releases.beta.json"),
+            );
 
-        copyFileSync(
-            fixture("AvaloniaCrossPlat-1.0.11-win-full.nupkg"),
-            path.join(feedDir, "AvaloniaCrossPlat-1.0.11-full.nupkg"),
-        );
+            copyFileSync(
+                fixture("AvaloniaCrossPlat-1.0.11-win-full.nupkg"),
+                path.join(feedDir, "AvaloniaCrossPlat-1.0.11-full.nupkg"),
+            );
 
-        const update = await um.checkForUpdatesAsync();
+            const update = await um.checkForUpdatesAsync();
 
-        console.log(
-            `about to download update from ${feedDir} to ${packagesDir} ...`,
-        );
-        await um.downloadUpdateAsync(update!, () => {
+            console.log(
+                `about to download update from ${feedDir} to ${packagesDir} ...`,
+            );
+            await um.downloadUpdateAsync(update!, () => {
+            });
+
+            expect(
+                existsSync(path.join(packagesDir, "AvaloniaCrossPlat-1.0.11-full.nupkg")),
+            ).toBe(true);
         });
-
-        expect(
-            existsSync(path.join(packagesDir, "AvaloniaCrossPlat-1.0.11-full.nupkg")),
-        ).toBe(true);
-        await shortDelay();
     });
 });
