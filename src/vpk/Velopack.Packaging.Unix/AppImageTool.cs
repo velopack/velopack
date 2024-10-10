65b0cd640d25b4e8cc5cb8d5399691a42950926e
Dan Balasescu <smoogipoo@smgi.me>
2024-10-10 21:56:58 +0900

    Set xz as the default AppImage compression algorithm
    Was the previous default prior to https://github.com/velopack/velopack/pull/243.

--- src/vpk/Velopack.Packaging.Unix/AppImageTool.cs
+++ src/vpk/Velopack.Packaging.Unix/AppImageTool.cs
@@ -7,7 +7,7 @@ namespace Velopack.Packaging.Unix;
 
 public class AppImageTool
 {
-    public const string DefaultCompressionAlgorithm = "zstd";
+    public const string DefaultCompressionAlgorithm = "xz";
     
     public static void CreateLinuxAppImage(string appDir, string outputFile, RuntimeCpu machine, ILogger logger, string compression)
     {
