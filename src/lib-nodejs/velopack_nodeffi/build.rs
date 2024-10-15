a69bdc22085cb06931fe3995271191f0068f6b43
Caelan Sayler <git@caesay.com>
2024-10-15 17:02:18 +0100

    Fix path for ts codegen

--- src/lib-nodejs/velopack_nodeffi/build.rs
+++ src/lib-nodejs/velopack_nodeffi/build.rs
@@ -5,7 +5,7 @@ use velopack::*;
 
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let bindings_dir = Path::new(&manifest_dir).join("..").join("..").join("src").join("bindings");
+    let bindings_dir = Path::new(&manifest_dir).join("..").join("src").join("bindings");
     UpdateInfo::export_all_to(&bindings_dir).unwrap();
     UpdateOptions::export_all_to(&bindings_dir).unwrap();
     VelopackLocatorConfig::export_all_to(&bindings_dir).unwrap();
