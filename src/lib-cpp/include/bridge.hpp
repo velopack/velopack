ab29f5d0c4e864f90a3df497806e16451cb26f95
Caelan Sayler <git@caesay.com>
2024-10-16 07:40:30 +0100

    wip new C++ library

---
+++ src/lib-cpp/include/bridge.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "rust/cxx.h"
+
+struct HookCallbackManager {
+    void install_hook(::rust::String app_version) const {};
+    void update_hook(::rust::String app_version) const {};
+    void obsolete_hook(::rust::String app_version) const {};
+    void uninstall_hook(::rust::String app_version) const {};
+    void firstrun_hook(::rust::String app_version) const {};
+    void restarted_hook(::rust::String app_version) const {};
+};
+
+struct DownloadCallbackManager {
+    void download_progress(int16_t progress) const {};
+};
+
+struct LoggerCallbackManager {
+    void log(::LogLevel level, ::rust::String message) const {};
+};
+        
