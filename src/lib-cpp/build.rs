ef34ceb08800d823cd3d3b437fa46eeb6fee91f0
Caelan Sayler <git@caesay.com>
2024-10-20 01:58:28 +0100

    Disable unsupported linker flag on clang

--- src/lib-cpp/build.rs
+++ src/lib-cpp/build.rs
@@ -1,10 +1,10 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .file("src/bridge.cc")
+        .warnings_into_errors(true)
         .flag_if_supported("/std:c++17")
         .flag_if_supported("/EHsc") // exception unwind handling
         .flag_if_supported("-Wno-unused-function") // allow unused functions
-        .warnings_into_errors(true)
         .define("VELOPACK_LIBC_EXPORTS", Some("1"))
         .std("c++17")
         .compile("velopack_libc");
@@ -13,5 +13,7 @@ fn main() {
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/bridge.hpp");
     println!("cargo:rerun-if-changed=src/bridge.cc");
+
+    #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-arg=/WHOLEARCHIVE:velopack_libc.lib");
 }
--- src/lib-cpp/src/lib.rs
+++ src/lib-cpp/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 #![allow(non_snake_case)]
 
 mod map;
