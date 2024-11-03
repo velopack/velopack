a173b33aaf67ebc33730fae4471392a05181ce91
Caelan Sayler <git@caesay.com>
2024-11-03 11:59:51 +0000

    Switch to using fcntl from flock on unix

--- src/lib-csharp/Util/LockFile.cs
+++ src/lib-csharp/Util/LockFile.cs
@@ -55,26 +55,76 @@ namespace Velopack.Util
             }
         }
 
-
-        [SupportedOSPlatform("linux")]
-        [SupportedOSPlatform("macos")]
-        [DllImport("libc", SetLastError = true)]
-        private static extern int flock(int fd, int operation);
-
-        private const int LOCK_SH = 1; // Shared lock
-        private const int LOCK_EX = 2; // Exclusive lock
-        private const int LOCK_NB = 4; // Non-blocking
-        private const int LOCK_UN = 8; // Unlock
-
         [SupportedOSPlatform("linux")]
         [SupportedOSPlatform("macos")]
         private void UnixExclusiveLock(int fd)
         {
-            int ret = flock(fd, LOCK_EX | LOCK_NB);
-            if (ret != 0) {
-                throw new IOException("flock returned error: " + ret);
+            int ret;
+            if (VelopackRuntimeInfo.IsLinux) {
+                var lockOpt = new linux_flock {
+                    l_type = F_WRLCK,
+                    l_whence = SEEK_SET,
+                    l_start = 0,
+                    l_len = 0, // 0 means to lock the entire file
+                    l_pid = 0,
+                };
+                ret = fcntl(fd, F_SETLK, ref lockOpt);
+            } else if (VelopackRuntimeInfo.IsOSX) {
+                var lockOpt = new osx_flock {
+                    l_start = 0,
+                    l_len = 0, // 0 means to lock the entire file
+                    l_pid = 0,
+                    l_type = F_WRLCK,
+                    l_whence = SEEK_SET,
+                };
+                Console.WriteLine("hello");
+                ret = fcntl(fd, F_SETLK, ref lockOpt);
+            } else {
+                throw new PlatformNotSupportedException();
+            }
+          
+            Console.WriteLine(ret);
+            if (ret == -1) {
+                int errno = Marshal.GetLastWin32Error();
+                throw new IOException($"fcntl F_SETLK failed, errno: {errno}", new Win32Exception(errno));
             }
         }
+
+        [SupportedOSPlatform("linux")]
+        [DllImport("libc", SetLastError = true)]
+        private static extern int fcntl(int fd, int cmd, ref linux_flock linux_flock);
+        
+        [SupportedOSPlatform("macos")]
+        [DllImport("libc", SetLastError = true)]
+        private static extern int fcntl(int fd, int cmd, ref osx_flock linux_flock);
+
+        [SupportedOSPlatform("linux")]
+        [StructLayout(LayoutKind.Sequential)]
+        private struct linux_flock
+        {
+            public short l_type; /* Type of lock: F_RDLCK, F_WRLCK, F_UNLCK */
+            public short l_whence; /* How to interpret l_start: SEEK_SET, SEEK_CUR, SEEK_END */
+            public long l_start; /* Starting offset for lock */
+            public long l_len; /* Number of bytes to lock */
+            public int l_pid; /* PID of the process blocking our lock (F_GETLK only) */
+        }
+        
+        [SupportedOSPlatform("macos")]
+        [StructLayout(LayoutKind.Sequential)]
+        private struct osx_flock
+        {
+            public long l_start; /* Starting offset for lock */
+            public long l_len; /* Number of bytes to lock */
+            public int l_pid; /* PID of the process blocking our lock (F_GETLK only) */
+            public short l_type; /* Type of lock: F_RDLCK, F_WRLCK, F_UNLCK */
+            public short l_whence; /* How to interpret l_start: SEEK_SET, SEEK_CUR, SEEK_END */
+        }
+
+        private const int F_SETLK = 6; /* Non-blocking lock */
+        private const short F_RDLCK = 0; /* Read lock */
+        private const short F_WRLCK = 1; /* Write lock */
+        private const short F_UNLCK = 2; /* Remove lock */
+        private const short SEEK_SET = 0;
 
         [SupportedOSPlatform("windows")]
         [DllImport("kernel32.dll", SetLastError = true)]
