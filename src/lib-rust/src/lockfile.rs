7fa74e2a504bffada08c56e2a4ceb19949bdf27b
Caelan Sayler <git@caesay.com>
2024-11-07 19:55:15 +0000

    Finally fix both rust/C# implementations so they work together on *nix

--- src/lib-rust/src/lockfile.rs
+++ src/lib-rust/src/lockfile.rs
@@ -1,117 +1,124 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Error, ErrorKind, Result};
+use std::fs::File;
+use std::io::Result;
 use std::path::PathBuf;
 
 /// A lock file that is used to prevent multiple instances of the application from running.
 /// This lock file is automatically released and deleted when the `LockFile` is dropped.
+#[allow(dead_code)]
 pub struct LockFile {
     file_path: PathBuf,
     file: Option<File>,
+    file_descriptor: Option<i32>,
 }
 
 impl LockFile {
     /// Creates a new `FileLock` with the given file path.
     pub fn try_acquire_lock<P: Into<PathBuf>>(path: P) -> Result<Self> {
         let path: PathBuf = path.into();
+
         crate::util::retry_io(|| {
-            let mut lock_file = LockFile {
-                file_path: path.clone(),
-                file: None,
-            };
-
-            let mut options = OpenOptions::new();
-            options.read(true).write(true).create(true).truncate(true);
-
             #[cfg(windows)]
             {
-                use std::os::windows::fs::OpenOptionsExt;
-                options.custom_flags(0x04000000); // FILE_FLAG_DELETE_ON_CLOSE
-                options.share_mode(0);
+                let file = Self::windows_exclusive_lock(&path)?;
+                let lock = LockFile {
+                    file_path: path.clone(),
+                    file: Some(file),
+                    file_descriptor: None,
+                };
+                Ok(lock)
             }
 
-            let file = options.open(&path)?;
-
-            #[cfg(any(target_os = "linux", target_os = "macos", target_os = "android"))]
+            #[cfg(unix)]
             {
-                use std::os::unix::io::AsRawFd;
-                let fd = file.as_raw_fd();
-                lock_file.unix_exclusive_lock(fd)?;
+                let fd = unsafe { Self::unix_exclusive_lock(&path)? };
+                let lock = LockFile {
+                    file_path: path.clone(),
+                    file: None,
+                    file_descriptor: Some(fd),
+                };
+                Ok(lock)
             }
-
-            #[cfg(target_os = "windows")]
-            {
-                use std::os::windows::io::AsRawHandle;
-                let handle = file.as_raw_handle();
-                lock_file.windows_exclusive_lock(handle)?;
-            }
-            lock_file.file = Some(file);
-            Ok(lock_file)
         })
     }
 
     /// Releases the lock and closes the file.
     fn dispose(&mut self) {
+        let _ = self.file.take(); // dispose file handle
+        #[cfg(unix)]
         {
-            let _ = self.file.take();
+            if let Some(fd) = self.file_descriptor.take() {
+                unsafe { libc::close(fd); }
+            }
         }
-        let _ = std::fs::remove_file(&self.file_path);
     }
 
     /// Acquires an exclusive, non-blocking lock on Unix-like systems.
     #[cfg(unix)]
-    fn unix_exclusive_lock(&self, fd: std::os::unix::io::RawFd) -> Result<()> {
-        use libc::{fcntl, F_SETLK, F_WRLCK, SEEK_SET};
+    unsafe fn unix_exclusive_lock<P: Into<PathBuf>>(path: P) -> Result<i32> {
+        use std::os::unix::ffi::OsStrExt;
+        use libc::{open, creat, lockf, close, F_TLOCK, O_RDWR, O_CLOEXEC, EINTR};
+        use std::io::{Error, ErrorKind};
 
-        let lock = libc::flock {
-            l_type: F_WRLCK as libc::c_short,
-            l_whence: SEEK_SET as libc::c_short,
-            l_start: 0,
-            l_len: 0, // 0 means to lock the entire file
-            l_pid: 0,
-        };
+        let path = path.into();
+        let c_path = std::ffi::CString::new(path.as_os_str().as_bytes())?;
 
-        let ret = unsafe { fcntl(fd, F_SETLK, &lock) };
+        // try to open existing file
+        let mut fd;
+        loop { 
+            fd = open(c_path.as_ptr(), O_RDWR | O_CLOEXEC);
+            if fd != -1 || Error::last_os_error().raw_os_error() != Some(EINTR) { break; }
+        }
+
+        // create it if that fails
+        if fd == -1 {
+            loop { 
+                fd = creat(c_path.as_ptr(), 0o666);
+                if fd != -1 || Error::last_os_error().raw_os_error() != Some(EINTR) { break; }
+            }
+        }
+
+        if fd == -1 {
+            let err = std::io::Error::last_os_error();
+            let _ = close(fd);
+            return Err(std::io::Error::new(
+                ErrorKind::Other,
+                format!("Failed to open lock file: {}", err),
+            ))
+        }
+
+        let mut ret;
+        loop { 
+            ret = lockf(fd, F_TLOCK, 0);
+            if ret != -1 || Error::last_os_error().raw_os_error() != Some(EINTR) { break; }
+        }
 
         if ret == -1 {
-            let err = Error::last_os_error();
-            Err(Error::new(
+            let err = std::io::Error::last_os_error();
+            let _ = close(fd);
+            Err(std::io::Error::new(
                 ErrorKind::Other,
                 format!("Failed to lock file: {}", err),
             ))
         } else {
-            Ok(())
+            Ok(fd)
         }
     }
 
     /// Acquires an exclusive, non-blocking lock on Windows systems.
     #[cfg(windows)]
-    fn windows_exclusive_lock(&self, handle: std::os::windows::io::RawHandle) -> Result<()> {
-        use windows::Win32::Foundation::HANDLE;
-        use windows::Win32::Storage::FileSystem::{LockFileEx, LOCKFILE_EXCLUSIVE_LOCK, LOCKFILE_FAIL_IMMEDIATELY};
-        use windows::Win32::System::IO::OVERLAPPED;
+    fn windows_exclusive_lock<P: Into<PathBuf>>(path: P) -> Result<File> {
+        use std::os::windows::fs::OpenOptionsExt;
+        use std::fs::OpenOptions;
 
-        let mut overlapped = OVERLAPPED::default();
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            // .custom_flags(0x04000000) // FILE_FLAG_DELETE_ON_CLOSE
+            .share_mode(0)
+            .open(path.into())?;
 
-        let res = unsafe {
-            LockFileEx(
-                HANDLE(handle.into()),
-                LOCKFILE_EXCLUSIVE_LOCK | LOCKFILE_FAIL_IMMEDIATELY,
-                0,
-                1,
-                0,
-                &mut overlapped,
-            )
-        };
-
-        if res.is_err() {
-            let err = Error::last_os_error();
-            return Err(Error::new(
-                ErrorKind::Other,
-                format!("Failed to lock file: {}", err),
-            ));
-        }
-
-        Ok(())
+        Ok(file)
     }
 }
 
