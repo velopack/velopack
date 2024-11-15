e67c60258dda15e141b27f3d81dd0e3ead61cee1
Caelan Sayler <git@caesay.com>
2024-11-15 22:09:10 +0000

    Also compile to static lib

--- src/lib-nodejs/src/load.ts
+++ src/lib-nodejs/src/load.ts
@@ -2,6 +2,7 @@ import { proxy } from "@neon-rs/load";
 
 module.exports = proxy({
   platforms: {
+    "win32-x86-msvc": () => require("./native/velopack_nodeffi_win_x86_msvc.node"),
     "win32-x64-msvc": () => require("./native/velopack_nodeffi_win_x64_msvc.node"),
     "win32-arm64-msvc": () => require("./native/velopack_nodeffi_win_arm64_msvc.node"),
     "darwin-x64": () => require("./native/velopack_nodeffi_osx.node"),
