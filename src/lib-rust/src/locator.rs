b7908189d8476e8d65fe927ae8400f02c40c6578
Kevin Bost <kitokeboo@gmail.com>
2024-11-21 00:18:41 -0800

    Starting work to move package directory based on install location

--- src/lib-rust/src/locator.rs
+++ src/lib-rust/src/locator.rs
@@ -1,9 +1,7 @@
 use std::path::PathBuf;
 use semver::Version;
 use crate::{
-    bundle::{self, Manifest},
-    util, Error,
-    lockfile::LockFile
+    bundle::{self, Manifest}, lockfile::LockFile, util::{self}, Error
 };
 
 /// Returns the default channel name for the current OS.
@@ -126,7 +124,12 @@ impl VelopackLocator {
 
     /// Returns the path to the current app's packages directory.
     pub fn get_packages_dir(&self) -> PathBuf {
-        self.paths.PackagesDir.clone()
+        let path = self.paths.PackagesDir.clone();
+        if self.is_local_machine_install() || !util::is_directory_writable(path) {
+            //TODO Need to add in the app name here.
+            //util::get_local_app_data()
+        }
+        path
     }
 
     /// Returns the path to the current app's packages directory as a string.
@@ -285,6 +288,15 @@ impl VelopackLocator {
         let lock_file_path = packages_dir.join(".velopack_lock");
         let lock_file = LockFile::try_acquire_lock(&lock_file_path)?;
         Ok(lock_file)
+    }
+
+    #[cfg(any(target_os = "windows", target_os = "macos"))]
+    /// Returns whether the current app is installed in one of the Program Files directory.
+    pub fn is_local_machine_install(&self) -> bool {
+        #[cfg(target_os = "windows")]
+        return self.paths.RootAppDir.starts_with("C:\\Program Files");
+        #[cfg(target_os = "macos")]
+        return self.paths.RootAppDir.starts_with("/Applications");
     }
 
     fn path_as_string(path: &PathBuf) -> String {
