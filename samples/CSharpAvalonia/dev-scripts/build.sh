f0a08884610610533274d3d88e6405bff2fa6f3c
Caelan Sayler <git@caesay.com>
2024-11-28 22:01:33 +0000

    Fix dev scripts

--- samples/CSharpAvalonia/dev-scripts/build.sh
+++ samples/CSharpAvalonia/dev-scripts/build.sh
@@ -18,8 +18,7 @@ cd "$SCRIPT_DIR/../../.."
 
 echo ""
 echo "Building Velopack Rust"
-cargo build --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/debug/update target/debug/update
+cargo build
 
 echo ""
 echo "Building Velopack Vpk"
@@ -27,9 +26,34 @@ dotnet build src/vpk/Velopack.Vpk/Velopack.Vpk.csproj
 
 cd "$SCRIPT_DIR/.."
 
+# Determine the default RID for the platform
+RID=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    ARCH=$(uname -m)
+    if [[ "$ARCH" == "x86_64" ]]; then
+        RID="osx-x64"
+    elif [[ "$ARCH" == "arm64" ]]; then
+        RID="osx-arm64"
+    fi
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    # Linux
+    ARCH=$(uname -m)
+    if [[ "$ARCH" == "x86_64" ]]; then
+        RID="linux-x64"
+    elif [[ "$ARCH" == "aarch64" ]]; then
+        RID="linux-arm64"
+    fi
+else
+    echo "Unsupported OS type: $OSTYPE"
+    exit 1
+fi
+
+echo "Using RID: $RID"
+
 echo ""
 echo "Compiling VelopackCSharpAvalonia with dotnet..."
-dotnet publish -c Release --self-contained -r linux-x64 -o "$PUBLISH_DIR" -p:UseLocalVelopack=true
+dotnet publish -c Release --self-contained -r "$RID" -o "$PUBLISH_DIR" -p:UseLocalVelopack=true
 
 echo ""
 echo "Building Velopack Release v$BUILD_VERSION"
