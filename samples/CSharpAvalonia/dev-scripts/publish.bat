9614e3d70b86285a161edee7e3aa3526ac875cbb
Caelan Sayler <git@caesay.com>
2024-12-27 11:34:52 +0000

    Clean up flow and add exception unwrapping

--- samples/CSharpAvalonia/dev-scripts/publish.bat
+++ samples/CSharpAvalonia/dev-scripts/publish.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal enabledelayedexpansion
 cd %~dp0..
-%~dp0..\..\..\build\Debug\net8.0\vpk publish -o releases
+%~dp0..\..\..\build\Debug\net8.0\vpk publish -o releases %*
--- src/vpk/Velopack.Flow/FlowApiExtensions.cs
+++ src/vpk/Velopack.Flow/FlowApiExtensions.cs
@@ -1,9 +1,40 @@
-﻿namespace Velopack.Flow;
+﻿using Velopack.Core;
+
+namespace Velopack.Flow;
 
 public partial class Profile
 {
     public string GetDisplayName()
     {
         return DisplayName ?? Email ?? "<unknown>";
+    }
+}
+
+public class ApiErrorResult
+{
+    public string? Type { get; set; }
+    public string? Title { get; set; }
+    public string? Detail { get; set; }
+    public int? Status { get; set; }
+
+    public UserInfoException? ToUserInfoException()
+    {
+        if (!String.IsNullOrWhiteSpace(Detail)) {
+            return new UserInfoException(Detail);
+        }
+
+        return null;
+    }
+}
+
+public static class FlowApiExtensions
+{
+    public static ApiErrorResult? ToErrorResult(this ApiException ex)
+    {
+        if (ex.Response != null) {
+            return SimpleJson.DeserializeObject<ApiErrorResult>(ex.Response);
+        }
+
+        return null;
     }
 }
