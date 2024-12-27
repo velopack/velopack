f0c3a100dc1494bf809f8a531b00307f6049e582
Kevin Bost <kitokeboo@gmail.com>
2024-12-26 22:16:24 -0800

    Rename generated file to have .g.cs extension

--- src/vpk/Velopack.Flow/FlowApi.gen.bat
+++ src/vpk/Velopack.Flow/FlowApi.gen.bat
@@ -1,2 +1,2 @@
 dotnet tool update --global NSwag.ConsoleCore
-nswag openapi2csclient /input:https://api.velopack.io/swagger/v1/swagger.json /output:FlowApi.cs /namespace:Velopack.Flow /classname:FlowApi
+nswag openapi2csclient /input:https://api.velopack.io/swagger/v1/swagger.json /output:FlowApi.g.cs /namespace:Velopack.Flow /classname:FlowApi
