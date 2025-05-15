d76529759004a788ae00ecd1a9650a50e6374d8f
Thomas Haid <thojaw@gmail.com>
2025-05-15 21:07:45 +0200

    Refactor downloader methods to use a dictionary for headers, has effect on Sources

--- src/lib-csharp/Sources/IFileDownloader.cs
+++ src/lib-csharp/Sources/IFileDownloader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,31 +15,26 @@ namespace Velopack.Sources
         /// </summary>
         /// <param name="url">The url which will be downloaded.</param>
         /// <param name="targetFile">
-        /// The local path where the file will be stored
-        /// If a file exists at this path, it will be overwritten.</param>
+        ///     The local path where the file will be stored
+        ///     If a file exists at this path, it will be overwritten.</param>
         /// <param name="progress">
-        /// A delegate for reporting download progress, with expected values from 0-100.
+        ///     A delegate for reporting download progress, with expected values from 0-100.
         /// </param>
-        /// <param name="authorization">
-        /// Text to be sent in the 'Authorization' header of the request.
-        /// </param>
-        /// <param name="accept">
-        /// Text to be sent in the 'Accept' header of the request.
-        /// </param>
+        /// <param name="headers">Headers that can be passed to Http Downloader, e.g. Accept or Authorization.</param>
         /// <param name="timeout">
-        /// The maximum time in minutes to wait for the download to complete.
+        ///     The maximum time in minutes to wait for the download to complete.
         /// </param>
         /// <param name="cancelToken">Optional token to cancel the request.</param>
-        Task DownloadFile(string url, string targetFile, Action<int> progress, string? authorization = null, string? accept = null, double timeout = 30, CancellationToken cancelToken = default);
+        Task DownloadFile(string url, string targetFile, Action<int> progress, IDictionary<string, string>? headers = null, double timeout = 30, CancellationToken cancelToken = default);
 
         /// <summary>
         /// Returns a byte array containing the contents of the file at the specified url
         /// </summary>
-        Task<byte[]> DownloadBytes(string url, string? authorization = null, string? accept = null, double timeout = 30);
+        Task<byte[]> DownloadBytes(string url, IDictionary<string, string>? headers = null, double timeout = 30);
 
         /// <summary>
         /// Returns a string containing the contents of the specified url
         /// </summary>
-        Task<string> DownloadString(string url, string? authorization = null, string? accept = null, double timeout = 30);
+        Task<string> DownloadString(string url, IDictionary<string, string>? headers = null, double timeout = 30);
     }
 }
