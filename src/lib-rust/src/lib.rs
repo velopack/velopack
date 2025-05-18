84256215431eb5031077a8c11c243848b604b82c
Caelan Sayler <git@caesay.com>
2025-05-18 22:54:28 +0100

    Fix tests and add size validation to rust

--- src/lib-rust/src/lib.rs
+++ src/lib-rust/src/lib.rs
@@ -127,8 +127,10 @@ pub enum Error
     FileNotFound(String),
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    #[error("Checusum did not match for {0} (expected {1}, actual {2})")]
-    Checksum(String, String, String),
+    #[error("Checksum did not match for {0} (expected {1}, actual {2})")]
+    ChecksumInvalid(String, String, String),
+    #[error("Size did not match for {0} (expected {1}, actual {2})")]
+    SizeInvalid(String, u64, u64),
     #[error("Zip error: {0}")]
     Zip(#[from] zip::result::ZipError),
     #[error("Network error: {0}")]
