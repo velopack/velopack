8cda06cfd7e360493503a338a5d7fabb6c6feadf
Caelan Sayler <git@caesay.com>
2025-06-14 15:57:59 +0100

    fix python versions

--- .github/set-nbgv-version.ps1
+++ .github/set-nbgv-version.ps1
@@ -4,6 +4,7 @@ param(
 
 $originalLocation = Get-Location
 
+# setting cargo workspace version
 $scriptDir = "$PSScriptRoot/.."
 $path = Join-Path $scriptDir "Cargo.toml"
 Write-Host "Setting version to $version"
@@ -17,9 +18,28 @@ Write-Host "Setting version to $version"
     }
 } | Set-Content $path
 
+# setting nodejs version
 Set-Location "$scriptDir/src/lib-nodejs"
 npm version $version --no-git-tag-version
 
+# setting pyproject.toml version
+$pyprojectPath = [IO.Path]::Combine($scriptDir, 'src', 'lib-python', 'pyproject.toml')
+
+$pythonVersion = $version
+if ($pythonVersion -match '-g') {
+    $pythonVersion = $pythonVersion -replace '-g', '.dev+'
+}
+
+(Get-Content $pyprojectPath) | ForEach-Object {
+    if ($_ -match '^version\s*=\s*".*"') {
+        $_ -replace '^version\s*=\s*".*"', "version = `"$pythonVersion`""
+    }
+    else {
+        $_
+    }
+} | Set-Content $pyprojectPath
+
+# copying README.md
 Copy-Item -Path "$scriptDir/README_NUGET.md" -Destination "$scriptDir/src/lib-nodejs/README.md" -Force
 Copy-Item -Path "$scriptDir/README_NUGET.md" -Destination "$scriptDir/src/lib-rust/README.md" -Force
 
