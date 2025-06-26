72af62649f1e0ffe081a79ac39f37734e2455d1d
Caelan Sayler <git@caesay.com>
2025-06-26 22:38:17 +0100

    Add unix build script and add to samples CI

--- samples/PythonWXPython/build.bat
+++ samples/PythonWXPython/build.bat
@@ -12,18 +12,21 @@ cd %~dp0
 echo.
 echo Installing dependencies
 uv sync
+if errorlevel 1 exit /b 1
 
 echo update_url = R"%~dp0releases" > _build_config.py
 
 echo.
 echo Building Python application
 uv run pyinstaller --onedir --windowed --noconfirm main.py
+if errorlevel 1 exit /b 1
 
 echo.
 echo Building Velopack Release v%~1
 mkdir publish 2>nul
 xcopy /E /I /Y dist\main\* publish\ >nul
 vpk pack --packId VelopackPythonSampleApp --mainExe main.exe -o releases --packDir publish --packVersion %*
+if errorlevel 1 exit /b 1
 
 echo.
 echo Cleaning up
