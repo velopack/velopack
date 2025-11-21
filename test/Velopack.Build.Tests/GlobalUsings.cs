8aa982afb2662ffea9d75eff167e462d34157c14
Kevin Bost <kitokeboo@gmail.com>
2025-11-21 00:42:57 -0800

    Streamlines VPK tool execution in build
    Removes `DotNetToolRunner` and `VpkToolResolver`, directly packages the `vpk` executable in `Velopack.Build.csproj`, updates `PackTask` and `PublishTask` to invoke the embedded `vpk`, and extracts shared process argument handling to `ProcessExtensions.Shared.cs`.

--- test/Velopack.Build.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.IO;
-global using System.Linq;
-global using System.Threading;
-global using System.Threading.Tasks;
-global using Xunit;
-global using Xunit.Abstractions;
