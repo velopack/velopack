0b92c65e08f3dbdb7161d3debbbb5590ba5a214e
Kevin Bost <kitokeboo@gmail.com>
2025-12-01 19:01:32 -0800

    Removes unused usings from build tasks
    Removes System.Collections.Generic from PackTask.cs and System, System.Collections.Generic, System.Threading, System.Threading.Tasks from PublishTask.cs.

--- src/vpk/Velopack.Build/PackTask.cs
+++ src/vpk/Velopack.Build/PackTask.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Framework;
 
 namespace Velopack.Build;
 
--- src/vpk/Velopack.Build/PublishTask.cs
+++ src/vpk/Velopack.Build/PublishTask.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Framework;
 
 namespace Velopack.Build;
 
